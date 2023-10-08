fe9c79788c9c9e5cb92b443e9f1735b5bf5e4dc7
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2023-10-08 13:47:36 +0000

    hw02 complete

--- hw02/hw01.py
+++ hw02/hw01.py
@@ -1,3 +1,51 @@
+#!/usr/bin/env python3
+# //////////////////////////////////////
+#   
+#   Author: Sean Hyacinthe
+#   Date: 12/11/23
+#
+# 	Runs an etch-sketch game using buttong connected as described in the 
+#   wiring diagram. Led's are physically wired to button to indicate a 
+#   sucessful press
+#
+# 	Wiring:	Yellow:     Pos 1 btn(P8_16) - Up
+#           Green :     Pos 2 btn(P9_14) - Down
+#           Red   :     Pos 3 btn(P9_16) - Left
+#           Bue   :     Pos 4 btn(P9_23) - Right
+
+
+import gpiod
+import sys
+
+
+# gpio d stuff
+
+CONSUMER='getset'
+CHIP='1'
+getoffsets=[14, 18 ,19, 17] 
+idx = [0,1,2,3]
+offset_cmd = ["w", "s", "a", "d"]
+           # W   S   A  D
+           # Y   G   R  B
+
+offset_2_cmd = dict(zip(idx,offset_cmd))
+def print_event(event):
+    if event.type == gpiod.LineEvent.FALLING_EDGE:
+        evstr = 'FALLING EDGE'
+    else:
+        raise TypeError('Invalid event type')
+
+    print('event: {} offset: {} timestamp: [{}.{}]'.format(evstr,
+                                                           event.source.offset(),
+                                                           event.sec, event.nsec))
+                  
+chip = gpiod.Chip(CHIP)
+
+getlines = chip.get_lines(getoffsets)
+getlines.request(consumer=CONSUMER, type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+
+
+
 # board constants
 BOARD_ROWS = 8
 BOARD_COLS = 10
@@ -40,8 +88,7 @@ def update_pos(r = cursor[0],c = cursor[1]) :
     if valid_move(r,c):
         cursor[0] = r
         cursor[1] = c
-        if sketch_board[cursor[0]][cursor[1]] != "X":
-            sketch_board[r][c] = "O"
+
     else:
        sketch_board[cursor[0]][cursor[1]] = "O"
     
@@ -53,9 +100,6 @@ def parse_cmd(cmd):
     elif pcmd == "clear":
         clear_board(sketch_board)
         
-    #preemptive update sketch board to remove old cursor location  if not drawn on  
-    if sketch_board[cursor[0]][cursor[1]] != "X":
-        sketch_board[cursor[0]][cursor[1]] = " "
     
     if pcmd == "w":
         update_pos(cursor[0]-1, cursor[1])
@@ -65,12 +109,25 @@ def parse_cmd(cmd):
         update_pos(cursor[0]+1, cursor[1])
     elif pcmd == "d":
         update_pos(cursor[0], cursor[1]+1)
-    elif pcmd == "x":
-        draw()
+
+    draw()
         
+
+def gpio_input():
+    ev_lines = getlines.event_wait(sec=1)
+    if ev_lines:
+        for line in ev_lines:
+            event = line.event_read()
+            #print_event(event)
+    vals = getlines.get_values()
+    return offset_2_cmd.get(vals.index(1)) if 1 in vals else ""
+
 update_pos()
 print_board(sketch_board)
+print("Pos 1 - up; Pos 2 - down; Pos 3 - left; Pos 4 - right;")
+
 while True:
-    cmd = input("w - up; a - left; d - right; s - down; clear - clear board; quit - quit game: ")
-    parse_cmd(cmd)
-    print_board(sketch_board)
+    cmd = gpio_input()
+    if cmd != "":
+        parse_cmd(cmd)
+        print_board(sketch_board)
